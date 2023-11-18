1b44154ab1d353f5c56b68fc57beb192c7c0ae31
Kilastly <kilastly@gmail.com>
2023-11-18 18:26:55 +0600

    005 сделали if

---
+++ example0005_ifElse/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите свое имя, смерд: ");
+string UserName = Console.ReadLine();
+
+if(UserName == "Ley")
+{
+    Console.WriteLine("Ура, это же LEY!");
+}
+else
+{
+    Console.Write("Уходи ");
+    Console.Write(UserName);
+    Console.Write(", ты смерд!");
+};
